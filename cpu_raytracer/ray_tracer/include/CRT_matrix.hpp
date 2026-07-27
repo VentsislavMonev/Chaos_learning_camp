f76e633be355d7fb0eac0cab7dc8ad225f3dbd46
VentsislavMonev <ventsislavmonevwork@gmail.com>
2026-07-28 00:33:49 +0300

    fixed matrix class so it can be initialized default with identity matrix but when doing arithmeticss its loaded with a matrix with 0s

--- cpu_raytracer/ray_tracer/include/CRT_matrix.hpp
+++ cpu_raytracer/ray_tracer/include/CRT_matrix.hpp
@@ -3,25 +3,42 @@
 
 #include "CRT_vector.hpp"
 
+// this shouldnt be changed its in a variable so it isnt a magic number
 static const int DIMENSIONS = 3;
 
 class CRT_matrix
 {
     
 public:
-    float matrix[DIMENSIONS][DIMENSIONS];
-
-    friend CRT_matrix operator*(const CRT_matrix& a, const CRT_matrix& b) 
+    CRT_matrix() noexcept
     {
-        CRT_matrix r{};
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                for (int k = 0; k < 3; k++)
-                    r.matrix[i][j] += a.matrix[i][k] * b.matrix[k][j];
-        return r;
+        matrix[0][0]=1.0f;matrix[0][1]=0.0f;matrix[0][2]=0.0f;
+        matrix[1][0]=0.0f;matrix[1][1]=1.0f;matrix[1][2]=0.0f;
+        matrix[2][0]=0.0f;matrix[2][1]=0.0f;matrix[2][2]=1.0f;
+    }
+    CRT_matrix( float a00,float a01,float a02,
+                float a10,float a11,float a12,
+                float a20,float a21,float a22) noexcept
+    {
+        matrix[0][0]=a00;matrix[0][1]=a01;matrix[0][2]=a02;
+        matrix[1][0]=a10;matrix[1][1]=a11;matrix[1][2]=a12;
+        matrix[2][0]=a20;matrix[2][1]=a21;matrix[2][2]=a22;
     }
 
-    friend CRT_vector operator*(const CRT_matrix& mat, const CRT_vector& v) 
+    friend CRT_matrix operator*(const CRT_matrix& a, const CRT_matrix& b) noexcept
+    {
+        CRT_matrix result(  0.0f,0.0f,0.0f,
+                            0.0f,0.0f,0.0f,
+                            0.0f,0.0f,0.0f);
+
+        for (int i = 0; i < DIMENSIONS; i++)
+            for (int j = 0; j < DIMENSIONS; j++)
+                for (int k = 0; k < DIMENSIONS; k++)
+                    result.matrix[i][j] += a.matrix[i][k] * b.matrix[k][j];
+        return result;
+    }
+
+    friend CRT_vector operator*(const CRT_vector& v, const CRT_matrix& mat) noexcept
     {
         return CRT_vector(
             mat.matrix[0][0] * v.x + mat.matrix[0][1] * v.y + mat.matrix[0][2] * v.z,
@@ -29,6 +46,9 @@ public:
             mat.matrix[2][0] * v.x + mat.matrix[2][1] * v.y + mat.matrix[2][2] * v.z
         );
     }
+
+public:
+    float matrix[DIMENSIONS][DIMENSIONS];
 };
 
 
